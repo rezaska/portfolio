ed436602f2da1753670d7a6b1a6d01a421f58da8
rezaska <reza.soleimani.ka@gmail.com>
2021-08-03 11:58:23 -0400

    fixed hamburger menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.onscroll = function () { scrollFunction() };
 
 // Get the navbar
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById('navbar');
 
 // Get the offset position of the navbar
 const sticky = navbar.offsetTop;
@@ -10,14 +10,20 @@ const sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add('sticky')
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky');
   }
 }
 
-const hamburgerMenu = document.getElementsByClassName("hamburger");
+const hamburgerMenu = document.querySelector('.hamburger');
 
-hamburgerMenu.addEventListener('click', function (event) {
-  alert('Element clicked through function!');
+hamburgerMenu.addEventListener('click', (e) => {
+  console.log('hello');
+  let menu = document.querySelector('.nav-text');
+  if (menu.style.display === 'none') {
+    menu.style.display = 'inline-block';
+  } else {
+    menu.style.display = 'none';
+  }
 });
