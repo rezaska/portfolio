3efa86feabab6aa320f13e2ce02943c40bdd1438
rezaska <reza.soleimani.ka@gmail.com>
2021-08-03 10:32:51 -0400

    scrollbar added + overall improvement design

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () { scrollFunction() };
 
 // Get the navbar
 const navbar = document.getElementById("navbar");
@@ -8,10 +8,16 @@ const navbar = document.getElementById("navbar");
 const sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function scrollFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+const hamburgerMenu = document.getElementsByClassName("hamburger");
+
+hamburgerMenu.addEventListener('click', function (event) {
+  alert('Element clicked through function!');
+});
